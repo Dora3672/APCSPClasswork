ea6994ad637ef0ec02d589e7657fee3fd3aa85e8
Dora3672 <112570832+Dora3672@users.noreply.github.com>
2022-10-21 12:47:44 -0700

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -7,18 +7,21 @@ Conversion Program
 1. Ask the user for the number that needs to be converted (get_int; do while loop, the number needs to be a positive integer)
 Message: "Please enter a number: "
 
-2. Create a function to find the max power of the desired base the number can hold (int findlength, needs the decimal number and the base of the converted number as inputs)
+2. Ask the user for the base the user wants to convert the number to (get_int, do while loop, the base needs to be 2, 8, or 16)
+Message: "Please enter the base you want to convert the number to: "
+
+3. Create a function to find the max power of the desired base the number can hold (int findlength, needs the decimal number and the base of the converted number as inputs)
     calculate the number of times the number can be divided by the base (needs to update the number after each division) (while loop; while the base to the the current time power is less than or equal to the number)
     return the max power
 
-3. Create a function to print the desirable array (void print, needs the array, the base of the converted number and the length of the array as inputs)
+4. Create a function to print the desirable array (void print, needs the array, the base of the converted number and the length of the array as inputs)
     for every number in the array, index starts from 0, index needs to be smaller than the length of the array, index++
         if the desired converted number is hex and the number is a two-digit number
             print the corresponding hex letter
         else if the printed digit is a number in the desirable base
             print the number in that index of the5432 array
 
-4. Create a function for converting the number into another base (void getArray, needs the decimal number and the base of the converted number as inputs)
+5. Create a function for converting the number into another base (void getArray, needs the decimal number and the base of the converted number as inputs)
     calculate the length the array (call the findlength function)
     create a new array with that length
     for each place in the array, place starts from 0, place needs to smaller than the length of the array, place++
@@ -26,10 +29,15 @@ Message: "Please enter a number: "
         update the num by num/base
     print the array (call the print function)
 
-5. Ask the user if they want to convert another number or not (get_char)
-Message:"Do you want to do another conversion? (Y/N) "
+6. Ask the user if they want to conver the same number into another base or not (get_char; do while loop, while the user says 'Y')
+Message:"Do you want to do another conversion with the same number? (Y/N) "
     if 'Y',
-    start again
+    do step 2 and call the function in step 5
+
+7. Ask the user if they want to convert another number or not (get_char; do while loop, while the user says 'Y')
+Message:"Do you want to do another conversion with another number? (Y/N) "
+    if 'Y',
+    do steps 1 and 2, call the function in step 5, and do step 6
 
 */
 
@@ -44,27 +52,36 @@ void print(int arr[], int type, int length); //function to print the array (the 
 int main(void)
 {
     int input; //variable for the number the user input
+    int base; //variable for the base the user input
+    char another; //variable for whether the user wants to convert the same number into another base
     char next; //variable for whether the user wants to convert another number or not
     do{
         do
         {
-            input = get_int("Please enter a number: "); //ask the user for an input
+            input = get_int("Please enter a decimal number: "); //ask the user for an input
         }
         while (input <= 0); //the input needs to be a positive integer
 
-        printf("\n%i convert to: \n", input); //print heading
+        do
+        {
+            do
+            {
+                base = get_int("Please enter the base you want to convert the number to: "); //ask the user for the base
+            }
+            while (base != 2 && base != 8 && base != 16); //the base needs to be 2, 8, or 16
 
-        //Binary
-        getArray(input, 2); //convert and print the converted binary number
+            printf("\n%i convert to: \n", input); //print heading
 
-        //Octal
-        getArray(input, 8); //convert and print the converted octal number
 
-        //Hex
-        getArray(input, 16); //convert and print the converted hex number
+            getArray(input, base); //convert and print the converted number with the desired base
+
+            printf("\n"); //for aesthetic
+            another = get_char("Do you want to do another conversion with the same number? (Y/N) "); //ask whether the user wants to convert the number to another base or not
+        }
+        while (another == 'Y'); //convert to another base if the user wants to
 
         printf("\n"); //for aesthetic
-        next = get_char("Do you want to do another conversion? (Y/N) "); //ask whether the user wants to convert another number or not
+        next = get_char("Do you want to do another conversion with another number? (Y/N) "); //ask whether the user wants to convert another number or not
         printf("\n"); //for aesthetic
     }
     while (next == 'Y'); //start again if the user wants to convert another number
@@ -92,7 +109,7 @@ int findlength(int num, int type)
     int count = 0; //variable for the max power
     while (pow(type, count) <= num) //the max power is biggest power of that base the number can hold
     {
-        count++; //update max power
+        count++;
     }
     return count; //return the max power
 }
@@ -110,43 +127,14 @@ void print(int arr[], int type, int length)
     }
     else if (type == 16)
     {
-        printf("Hex: "); 
+        printf("Hex: ");
     }
     // printing the number, for each digit in the number, starting from the first one
     for(int digit = 0; digit < length; digit++) //loop is the variable for the current digit that is being printed
     {
-        //if the hex number needs to print a letter, convert the number into the corresponding hex number
-        if (type == 16 && arr[digit]>9)
-        {
-            if (arr[digit] == 10)
-            {
-                printf("A");
-            }
-            else if (arr[digit] == 11)
-            {
-                printf("B");
-            }
-            else if (arr[digit] == 12)
-            {
-                printf("C");
-            }
-            else if (arr[digit] == 13)
-            {
-                printf("D");
-            }
-            else if (arr[digit] == 14)
-            {
-                printf("E");
-            }
-            else if (arr[digit] == 15)
-            {
-                printf("F");
-            }
-        }
-        else //if the digit does not need hex conversion to letter
-        {
-            printf("%d", arr[digit]); //print the digit
-        }
+        char hex[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'}; // hex is the hex conversion dictionary
+        printf("%c", hex[arr[digit]]); //print the digit
+
     }
     printf("\n"); //for aesthetic
 }
